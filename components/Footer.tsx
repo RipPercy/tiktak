af4d1469204222a663613b474075d3a3fe97fa42
RipPercy <Hajooka2015@gmail.com>
2022-11-24 17:19:12 +0700

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import { NextPage } from 'next';
+
+const List = ({ items, mt }: { items: string[], mt: Boolean }) => (
+  <div className={`flex flex-wrap gap-2 ${mt && "mt-5"}`}>
+    {items.map((item: string) => (
+      <p key={item} className="text-gray-400 text-sm  hover:underline cursor-pointer" >
+        {item}
+      </p>
+    ))}
+  </div>
+);
+
+const Footer: NextPage = () => (
+  <div className="mt-6 hidden xl:block">
+    <p className="text-gray-400 text-sm mt-5">© 2022 TikTik</p>
+  </div>
+);
+
+export default Footer;
